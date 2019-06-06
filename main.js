400e1f6851dfdabc4532f6d53c6c0ee0f277511e
Long <fchbyoung1010@gmail.com>
2019-06-06 15:25:18 +0700

    tìm min của mảng

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
-function timMinTrongMang() {
-    let arr = [3,5,1,8,-3,7,8];
+function minArray(arr) {
     let min = arr[0];
-    for(let i = 1; i < arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(arr[i] < min){
             min = arr[i];
         }
     }
-    document.write("giá trị nhỏ nhất trong mảng " +"["+ arr +"]"+" là " + min);
+    return min;
 }
-timMinTrongMang();
+
+let arr1 = [3,5,1,8,-3,7,8];
+let min1 = minArray(arr1);
+document.write("giá trị nhỏ nhất trong mảng " + "[" +arr1 + "] là : " + min1);
+
+document.write("<br/>");
+
+let arr2 = [7,12,6,9,20,56,89];
+let min2 = minArray(arr2);
+document.write("giá trị nhỏ nhất trong mảng " + "[" +arr2 + "] là : " + min2);
+
+function minArray2(arr) {
+    if (arr.length == 0)
+        return -1;
+    let min = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+document.write("<br/>");
+
+let arr3 = [];
+let min3 = minArray2(arr3);
+document.write("giá trị nhỏ nhất trong mảng " + "[" +arr3 + "] là : " + min3);
+
+document.write("<br/>");
+
+let arr4 = [0,0,0,0,0,0];
+let min4 = minArray2(arr4);
+document.write("giá trị nhỏ nhất trong mảng " + "[" +arr4 + "] là : " + min4);
